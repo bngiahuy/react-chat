e9a0ba71e7aba6e9ff82b1259c8589a9ebb27f8d
Huy Bùi <45910202+bngiahuy@users.noreply.github.com>
2024-06-09 22:50:07 +0700

    add: notification function

---
+++ src/components/notification/Notification.jsx
@@ -0,0 +1,23 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const Notification = () => {
+	return (
+		<div>
+			<ToastContainer
+				position="top-right" // top-left, top-center, top-right, bottom-left, bottom-center, bottom-right
+				autoClose={3000} // 3 seconds
+				hideProgressBar={false}
+				newestOnTop={false} // place new notification on top
+				closeOnClick // close notification when clicked
+				rtl={false} // right to left
+				pauseOnFocusLoss // pause when focus is lost
+				draggable // allow dragging
+				pauseOnHover // pause when hovered
+				theme="light" // dark or light
+				transition:Slide // transition effect
+			/>
+		</div>
+	);
+};
+
+export default Notification;
