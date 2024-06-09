27ebb3d381ac77201e24f963d2a03b0a01b0d662
Huy Bùi <45910202+bngiahuy@users.noreply.github.com>
2024-06-09 21:10:36 +0700

    add: detail structure

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,10 +1,75 @@
-import "./detail.css";
+import './detail.css';
 const Detail = () => {
-    return (
-        <div className="detail">
-        Detail
-        </div>
-    );
-}
+	return (
+		<div className="detail">
+			<div className="user">
+				<img src="./avatar.png" alt="" />
+				<span className="name">An Bui</span>
+				<p className="status">Online</p>
+			</div>
+
+			<div className="info">
+				<div className="option">
+					<div className="title">
+						<span>Chat settings</span>
+						<img src="./arrowUp.png" alt="" />
+					</div>
+				</div>
+				<div className="option">
+					<div className="title">
+						<span>Help</span>
+						<img src="./arrowUp.png" alt="" />
+					</div>
+				</div>
+				<div className="option">
+					<div className="title">
+						<span>Photos</span>
+						<img src="./arrowDown.png" alt="" />
+					</div>
+
+					<div className="photos">
+						<div className="photoItems">
+							<div className="photoDetails">
+								<img src="/bg.png" alt="" />
+								<span>abc.png</span>
+							</div>
+							<img src="./download.png" alt="" className="downloadIcon" />
+						</div>
+						<div className="photoItems">
+							<div className="photoDetails">
+								<img src="/bg.png" alt="" />
+								<span>abc.png</span>
+							</div>
+							<img src="./download.png" alt="" className="downloadIcon" />
+						</div>
+						<div className="photoItems">
+							<div className="photoDetails">
+								<img src="/bg.png" alt="" />
+								<span>abc.png</span>
+							</div>
+							<img src="./download.png" alt="" className="downloadIcon" />
+						</div>
+						<div className="photoItems">
+							<div className="photoDetails">
+								<img src="/bg.png" alt="" />
+								<span>abc.png</span>
+							</div>
+							<img src="./download.png" alt="" className="downloadIcon" />
+						</div>
+					</div>
+				</div>
+				<div className="option">
+					<div className="title">
+						<span>Files</span>
+						<img src="./arrowUp.png" alt="" />
+					</div>
+				</div>
+
+				<button className="blockBtn">Block</button>
+				<button className="logout">Log out</button>
+			</div>
+		</div>
+	);
+};
 
 export default Detail;
