27548fb092fbf7a92da91266db92db79ad2d936f
Huy Bùi <45910202+bngiahuy@users.noreply.github.com>
2024-06-10 16:24:32 +0700

    add: chatID

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,13 +1,19 @@
+import { useEffect, useState } from 'react';
+import useChatStore from '../../libs/chatstore';
 import Bottom from './bottom/Bottom';
 import Center from './center/Center';
 import './chat.css';
 import Top from './top/Top';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '../../libs/firebase';
 const Chat = () => {
+	const { chatID } = useChatStore();
+
 	return (
 		<div className="chat">
-			<Top />
-			<Center />
-			<Bottom />
+			<Top chatID={chatID} />
+			<Center chatID={chatID} />
+			<Bottom chatID={chatID} />
 		</div>
 	);
 };
